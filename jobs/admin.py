8e1c24a42fbe83c96bdb8e0df5e8de7aebcc9d3c
faridfarhat <93440204+faridfarhat@users.noreply.github.com>
2024-08-25 13:59:58 +1200

    jobs app up and running

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+# Note below added
+from .models import Job
+admin.site.register(Job)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Note new below code added
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
